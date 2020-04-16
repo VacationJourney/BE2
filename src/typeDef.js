cd89985c8fb15468cbc4f0ecf415ea51ec7e2a5c
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-04-16 08:58:29 -0700

    update typeDefs

--- src/typeDef.js
+++ src/typeDef.js
@@ -4,15 +4,40 @@ const typeDefs = gql`
   type User {
   id: ID!
   username: String!
+  name: String
+  email: String
+  vacations: [Vacation!]!
+}
+
+type Vacation {
+  id: ID!
+  title: String!
+  startDate: DateTime!
+  endDate: DateTime!
+  events: [Event!]!
+}
+
+type Event {
+  id: ID! 
+  date: DateTime!
+  startTime: DateTime
+  endTime: DateTime
+  title: String!
+  description: String
 }
 
 type Query {
   currentUser: User!
+  userVacations: [Vacation!]!
+  currentVacation: Vacation!
+  dayEvents: [Event!]!
+  eventInfo: Event!
 }
 
 type Mutation {
-  signUp(username: String!, password: String!): User!
+  signUp(username: String!, name: String, email: String, password: String!): User!
   login(username: String!, password: String!): LoginResponse!
+  createVacation(title: String!, startDate: DateTime!, endDate: DateTime!, )
 }
 
 type LoginResponse {
