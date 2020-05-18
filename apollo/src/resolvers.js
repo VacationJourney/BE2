1fe027cf4f489fa7bdd3e0f74a1f28586c92770f
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-05-18 16:20:25 -0700

    move resolvers to apollo folder

---
+++ apollo/src/resolvers.js
@@ -0,0 +1,154 @@
+const bcrypt = require('bcryptjs');
+const signToken = require('./signToken');
+
+const resolvers = {
+	Mutation: {
+		// For the Users
+		signUp: async (parent, { username, name, email, password }, ctx, info) => {
+			const hashedPassword = await bcrypt.hash(password, 10);
+			const user = await ctx.prisma.createUser({
+				username,
+				name,
+				email,
+				password: hashedPassword,
+			});
+			return user;
+		},
+		login: async (parent, { username, password }, ctx, info) => {
+			const user = await ctx.prisma.user({ username });
+
+			if (!user) {
+				throw new Error('Invalid Login');
+			}
+
+			const passwordMatch = await bcrypt.compare(password, user.password);
+
+			if (!passwordMatch) {
+				throw new Error('Invalid Login');
+			}
+
+			const token = signToken(user);
+
+			return {
+				token,
+				user,
+			};
+		},
+
+		updateUser: async (
+			parent,
+			{ id, username, name, email, password },
+			{ prisma },
+			info
+		) => {
+			const hashedPassword = await bcrypt.hash(password, 10);
+			const changes = await prisma.updateUser({
+				data: { username, name, email, password: hashedPassword },
+				where: { id },
+			});
+			return changes;
+		},
+
+		deleteUser: async (parent, args, { user, prisma }, info) => {
+			return prisma.deleteUser({ id: user.id });
+		},
+
+		// for the vacations
+		createVacation: async (parent, args, { prisma }, info) => {
+			const vacation = await prisma.createVacation(args.data);
+			return vacation;
+		},
+
+		updateVacation: async (
+			parent,
+			{ id, title, startDate, endDate },
+			{ prisma },
+			info
+		) => {
+			const changes = await prisma.updateVacation({
+				data: { title, startDate, endDate },
+				where: { id },
+			});
+			return changes;
+		},
+
+		deleteVacation(parent, { id }, { prisma }, info) {
+			return prisma.deleteVacation({ id }, info);
+		},
+
+		// for the events
+		createEvent: async (parent, args, { prisma }, info) => {
+			const event = await prisma.createEvent({
+				date: args.date,
+				title: args.title,
+				trip: {
+					connect: { id: args.vacationId },
+				},
+			});
+			return event;
+		},
+
+		updateEvent: async (
+			parent,
+			{ id, date, startTime, endTime, title, description },
+			{ prisma },
+			info
+		) => {
+			const event = await prisma.updateEvent({
+				data: { date, startTime, endTime, title, description },
+				where: { id },
+			});
+			return event;
+		},
+		deleteEvent(parent, { id }, { prisma }, info) {
+			return prisma.deleteEvent({ id }, info);
+		},
+	},
+	Query: {
+		currentUser: (parent, args, { user, prisma }) => {
+			// this if statement is our authentication check
+			if (!user) {
+				throw new Error('Not Authenticated');
+			}
+			return prisma.user({ id: user.id });
+		},
+		vacations: (parent, args, { user, prisma }) => {
+			return prisma.user({ id: user.id }).vacations();
+		},
+		vacation: (parent, { id }, { prisma }) => {
+			return prisma.vacation({ id });
+		},
+		event: (parent, { id }, { prisma }) => {
+			return prisma.event({ id });
+		},
+	},
+	User: {
+		vacations(parent, args, { prisma }) {
+			return prisma
+				.user({
+					id: parent.id,
+				})
+				.vacations();
+		},
+	},
+	Vacation: {
+		events(parent, args, { prisma }) {
+			return prisma
+				.vacation({
+					id: parent.id,
+				})
+				.events();
+		},
+	},
+	Event: {
+		trip(parent, args, { prisma }) {
+			return prisma
+				.event({
+					id: parent.id,
+				})
+				.trip();
+		},
+	},
+};
+
+module.exports = resolvers;
