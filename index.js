e48749181f788b5fbbf2cdaccf0ab6df70737cac
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-05-18 16:18:58 -0700

    update index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+
+const { ApolloServer, gql } = require('apollo-server');
+const { importSchema } = require('graphql-import')
+
+const resolvers = require('./apollo/src/resolvers');
+const { prisma } = require('../apollo/src/generated/prisma-client');
+const jwt = require('jsonwebtoken')
+
+const typeDefs = await importSchema('apollo/schema/schema.graphql')
+
+const getUser = token => {
+  try {
+    if (token) {
+      return jwt.verify(token, process.env.JWT_SECRET)
+    }
+    return null
+  } catch (err) {
+    return null
+  }
+}
+
+const server = new ApolloServer({
+    typeDefs: gql(typeDefs),
+  resolvers,
+  context: ({ req }) => {
+    const tokenWithBearer = req.headers.authorization || ''
+    const token = tokenWithBearer.split(' ')[1]
+    const user = getUser(token)
+
+    return {
+      user,
+      prisma, // the generated prisma client if you are using it
+    }
+  },
+})
+
+server
+  .listen({
+    port: 4000
+  })
+  .then(info => console.log(`Server started on http://localhost:${info.port} 🚀`));
