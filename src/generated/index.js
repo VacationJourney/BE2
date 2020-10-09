97cab87de57a3303c4c8c6db8135a4d1dd6d46b9
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-10-09 08:38:08 -0700

    Deploy a new Prisma data model for testing and dev

--- src/generated/index.js
+++ src/generated/index.js
@@ -7,6 +7,18 @@ var models = [
   {
     name: "User",
     embedded: false
+  },
+  {
+    name: "Vacation",
+    embedded: false
+  },
+  {
+    name: "Day",
+    embedded: false
+  },
+  {
+    name: "Event",
+    embedded: false
   }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
