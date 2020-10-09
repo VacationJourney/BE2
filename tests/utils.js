cd153e49a952fd0dd4fecafa96af0e52a68769db
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-10-09 08:41:49 -0700

    create authClient logic in tests

--- tests/utils.js
+++ tests/utils.js
@@ -4,3 +4,19 @@ export const client = new ApolloClient({
   uri: 'http://localhost:4000/',
   onError: (e) => { console.log(e) },
 });
+
+export const getClient = (token) => {
+  return new ApolloClient({
+   uri: 'http://localhost:4000/',
+   request: (operation) => {
+     if(token) {
+       operation.setContext({
+         headers: {
+           "Authorization": `Bearer ${token}`
+         }
+       })
+     }
+   },
+   onError: (e) => { console.log(e) },
+ });
+}
