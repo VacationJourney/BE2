aac0d4964d20a12f79ab40278d98984a129a729c
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-10-11 09:05:08 -0700

    fix server listen for port

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // import server from './src/app.js';
 const server = require('./src/app');
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+// server.listen().then(({ url }) => {
+//   console.log(`🚀  Server ready at ${url}`);
+// });
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+	console.log(`🚀 Server ready at ${url}`);
 });
