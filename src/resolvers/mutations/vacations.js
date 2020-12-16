47f3cc98d34ad1797c8803ca5bd2812552739106
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-12-16 15:07:37 -0800

    change createVacations and updateVacations in resolvers for budget

--- src/resolvers/mutations/vacations.js
+++ src/resolvers/mutations/vacations.js
@@ -1,32 +1,38 @@
-const {decodeToken} = require('../../utils/token');
+const { decodeToken } = require('../../utils/token');
 
-const createVacation =  async (parent, args, { prisma, req }, info) => {
-  const { data: { title, dates } } = args;
-  const {id } = decodeToken(req)
+const createVacation = async (parent, args, { prisma, req }, info) => {
+  const { data: { title, budget, dates } } = args;
+  const { id } = decodeToken(req)
   const vacation = await prisma.createVacation({
     title,
+    budget,
     dates,
     traveler: {
-      connect: {id}
+      connect: { id }
     }
-}, info);
-return vacation;
+  }, info);
+  return vacation;
 }
 
-const updateVacation =  async (parent, args, { prisma, req }, info) => {
+const updateVacation = async (parent, args, { prisma, req }, info) => {
+
   await prisma.deleteManyDays()
-  const {data: {title, dates}, where: {id}} = args;
-  const {username } = decodeToken(req)
+  const { data: { title, budget, cost, dates }, where: { id } } = args;
+  const { username } = decodeToken(req)
   const updatedVacation = await prisma.updateVacation({
-  data: {title,
-  dates,
-  traveler: {
-    connect: {username}
-  }},
-  where: {id: id}
-  
-})
-return updatedVacation;
+    data: {
+      title,
+      dates,
+      budget,
+      cost,
+      traveler: {
+        connect: { username }
+      }
+    },
+    where: { id: id }
+
+  })
+  return updatedVacation;
 }
 
 const deleteVacation = (parent, args, { prisma }, info) => {
@@ -37,4 +43,4 @@ const deleteDay = (parent, args, { prisma }, info) => {
   return prisma.deleteDay(args);
 }
 
-module.exports = {createVacation,updateVacation,deleteVacation,deleteDay}
+module.exports = { createVacation, updateVacation, deleteVacation, deleteDay }
