fdee562f9a38b0618357aa76426cafac71c6cff2
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-12-27 17:47:17 -0800

    add resolvers to resolver index

--- src/resolvers/mutations/index.js
+++ src/resolvers/mutations/index.js
@@ -1,11 +1,11 @@
-const {signUp, login, updateUser, deleteUser} = require('./user');
-const {createVacation,updateVacation,deleteVacation,deleteDay} = require('./vacations');
-const {createEvent,updateEvent,deleteEvent} = require('./events');
+const { signUp, login, updateUser, deleteUser } = require('./user');
+const { createVacation, updateVacation, deleteVacation, updateDayCost, updateVacationCost, deleteDay } = require('./vacations');
+const { createEvent, updateEvent, deleteEvent } = require('./events');
 
 const Mutation = {
   signUp, login, updateUser, deleteUser,
-  createVacation,updateVacation,deleteVacation,deleteDay,
-  createEvent,updateEvent,deleteEvent
+  createVacation, updateVacation, deleteVacation, updateDayCost, updateVacationCost, deleteDay, createEvent,
+ updateEvent, deleteEvent
 }
 
 module.exports = Mutation
