10fa3e2d6e626ba3e8574775e486601647ef50f4
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-01-08 14:09:27 -0800

    fix update mutation resolver. Add createDay mutation

--- src/resolvers/mutations/index.js
+++ src/resolvers/mutations/index.js
@@ -1,10 +1,10 @@
 const { signUp, login, updateUser, deleteUser } = require('./user');
-const { createVacation, updateVacation, deleteVacation, updateDayCost, updateVacationCost, deleteDay } = require('./vacations');
+const { createVacation, updateVacation,  deleteVacation, updateDayCost, updateVacationCost, createDay, deleteDay, deleteManyDays } = require('./vacations');
 const { createEvent, updateEvent, deleteEvent } = require('./events');
 
 const Mutation = {
   signUp, login, updateUser, deleteUser,
-  createVacation, updateVacation, deleteVacation, updateDayCost, updateVacationCost, deleteDay, createEvent,
+  createVacation, updateVacation, deleteVacation, updateDayCost, updateVacationCost, createDay, deleteDay, deleteManyDays, createEvent,
  updateEvent, deleteEvent
 }
 
